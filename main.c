09494d04bca816e7500fbeaa9a013ff9fe97cfc3
jpcirqueira <jpcirqueira81@gmail.com>
2021-03-21 20:13:55 -0300

    ajustes na main.

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 #define SOLICITA_TEMP_INTERNA 0xC1
 #define SOLICITA_TEMP_POT 0xC2
 
-int usateterminal=0;
+int usateterminal=0,escrevearquivo=0,resistor=0,ventoinha=0;
 float teTerminal=0;
 void paragpio(int sinal) {
   gpio_desliga(4); //desliga resistencia
@@ -24,36 +24,57 @@ void paragpio(int sinal) {
   exit(1);
 }
 void manda_alarm(int sinal){
+  escrevearquivo+=1;
   alarm(1);
 }
 
 void cria_arquivo(){
   FILE *file = fopen("./grafico/grafico.csv", "w");
-  fprintf(file,"TE,TI,TR,PID,DATA,HORA\n");
+  fprintf(file,"TE,TI,TR,RESISTOR,VENTOINHA,DATA,HORA\n");
 }
 
-void escreve_arquivo(float te, float ti, float tr, int pid){
+void escreve_arquivo(float te, float ti, float tr,int pid){
     FILE *file = fopen("./grafico/grafico.csv", "a");
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    fprintf(file,"%f,%f,%f,%d,%d/%d/%d, %02d:%02d:%02d\n", te,ti,tr,pid,tm.tm_mday,tm.tm_mon+ 1,tm.tm_year+ 1900,tm.tm_hour, tm.tm_min, tm.tm_sec);
+    if(pid<0){
+      ventoinha = pid*-1;
+      fprintf(file,"%f,%f,%f,%d,%d,%d/%d/%d, %02d:%02d:%02d\n", te,ti,tr,0,ventoinha,tm.tm_mday,tm.tm_mon+ 1,tm.tm_year+ 1900,tm.tm_hour, tm.tm_min, tm.tm_sec);
+    }else{
+      fprintf(file,"%f,%f,%f,%d,%d,%d/%d/%d, %02d:%02d:%02d\n", te,ti,tr,pid,0,tm.tm_mday,tm.tm_mon+ 1,tm.tm_year+ 1900,tm.tm_hour, tm.tm_min, tm.tm_sec);
+    }
     fclose(file);
 }
 
 void menu(){
-  printf("----------------------------------Trabalho1---------------------------------\n");
-  printf("|digite 1 se deseja inserir uma temperatura de referencia.                 |\n");
-  printf("|digite 2 para pegar a temperatura de referencia do potenciometro.         |\n");
-  printf("|pressione ctrl + d definir a temperatura de referencia durante a execucao.|\n");
-  printf("|pressione ctrl + c para parar finalizar execucao.                         |\n");
-  printf("----------------------------------------------------------------------------\n");
+  printf("-----------------------------------Trabalho1---------------------------------\n");
+  printf("|digite 1 se deseja inserir uma temperatura de referencia.                  |\n");
+  printf("|digite 2 para pegar a temperatura de referencia do potenciometro.          |\n");
+  printf("|pressione ctrl + z para mudar o modo de de pegar temperatura de referencia.|\n");
+  printf("|pressione ctrl + c para parar finalizar execucao.                          |\n");
+  printf("-----------------------------------------------------------------------------\n");
+  int entrada;
+  scanf("%d",&entrada);
+  if(entrada == 1){
+    printf("digite a temperatura de referencia:\n");
+    scanf("%f",&teTerminal);
+    usateterminal=1;
+  }else if(entrada == 2){
+    usateterminal=0;
+
+  }
 }
 
 void pegaTEterminal(int sinal){
   if(sinal == SIGTSTP){
-    usateterminal=1;
-    printf("digite a temperatura de referencia:\n");
-    scanf("%f",&teTerminal);
+    if(usateterminal == 0){// esta pegando potenciomentro
+      usateterminal=1;
+      printf("digite a temperatura de referencia:\n");
+      scanf("%f",&teTerminal);
+    }else if(usateterminal == 1){// esta pegando do terminal
+      usateterminal=0;
+    }
+    
   }else{
     exit(0);
   }
@@ -83,15 +104,16 @@ int main(){
     lcd(te, ti, tr); //escreve no lcd
     pid_atualiza_referencia(tr);
     int res_pid = (int) pid_controle(ti);
-    escreve_arquivo(te,ti,tr,res_pid);
+    if(escrevearquivo == 2){
+      escreve_arquivo(te,ti,tr,res_pid);
+      escrevearquivo = 0;
+    }
     printf("TE:%f TI:%f TR:%f PID:%d\n",te,ti,tr,res_pid); //printa as temperaturas
     if(res_pid > 0){
-      printf("%d\n",res_pid);
       gpio_liga(4,res_pid); //liga resistencia
       gpio_desliga(5); //desliga ventoinha
     }else if(res_pid < -40){
       res_pid= res_pid *-1;
-      printf("%d\n",res_pid);
       gpio_liga(5,res_pid); //liga ventoinha
       gpio_desliga(4); //desliga resistencia
     }else{
